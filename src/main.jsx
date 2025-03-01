67d16dcf03e329c78f3a303517c9c70052886bdb
spilody <fredy.aryadi.tekno@gmail.com>
2025-03-01 17:09:57 +0700

    +

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 dayjs.extend(duration)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
+      <SpeedInsights />
       <App />
     </ChakraProvider>
   </StrictMode>,
